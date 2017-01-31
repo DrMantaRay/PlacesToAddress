263e429bf3ca1de27e758d51daaa66dd97cc9f5f
DrMantaRay <patrick.chen64@gmail.com>
2017-01-31 00:27:45 -0800

    2nd commit

--- src/PlaceToAddress.java
+++ src/PlaceToAddress.java
@@ -10,7 +10,35 @@ import java.util.List;
 public class PlaceToAddress {
     public static void main(String[] args) {
         GooglePlaces client= new GooglePlaces("AIzaSyDeziui3HYOmTYeYAuZ3uULCqjLd1afk-E");
-        List<Place> places=client.getPlacesByQuery("Empire State Building",GooglePlaces.MAXIMUM_RESULTS);
-        System.out.println(places[0]);
+        List<Place> places = client.getPlacesByQuery("library", GooglePlaces.MAXIMUM_RESULTS);
+        int i=0;
+        for (Place place : places) {
+            System.out.println(place.getName());
+            i++;
+            if (i>20) {break;}
+          /*  if (place.getName().equals("Empire State Building")) {
+                empireStateBuilding = place;
+                break;
+            } */
+
+        }
+/*
+        if (empireStateBuilding != null) {
+            Place detailedEmpireStateBuilding = empireStateBuilding.getDetails(); // sends a GET request for more details
+            // Just an example of the amount of information at your disposal:
+           // System.out.println("ID: " + detailedEmpireStateBuilding.getId());
+            System.out.println("Name: " + detailedEmpireStateBuilding.getName());
+            System.out.println("Phone: " + detailedEmpireStateBuilding.getPhoneNumber());
+            System.out.println("International Phone: " + empireStateBuilding.getInternationalPhoneNumber());
+            System.out.println("Website: " + detailedEmpireStateBuilding.getWebsite());
+            System.out.println("Always Opened: " + detailedEmpireStateBuilding.isAlwaysOpened());
+            System.out.println("Status: " + detailedEmpireStateBuilding.getStatus());
+            System.out.println("Google Place URL: " + detailedEmpireStateBuilding.getGoogleUrl());
+            System.out.println("Price: " + detailedEmpireStateBuilding.getPrice());
+            System.out.println("Address: " + detailedEmpireStateBuilding.getAddress());
+            System.out.println("Vicinity: " + detailedEmpireStateBuilding.getVicinity());
+            System.out.println("Reviews: " + detailedEmpireStateBuilding.getReviews().size());
+            System.out.println("Hours:\n " + detailedEmpireStateBuilding.getHours());
+        } */
     }
 }
