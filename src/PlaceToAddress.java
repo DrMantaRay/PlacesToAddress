c78107efb242cd1e4874b5a308b84a441f11e618
DrMantaRay <patrick.chen64@gmail.com>
2017-02-02 01:34:46 -0800

    c

--- src/PlaceToAddress.java
+++ src/PlaceToAddress.java
@@ -1,10 +1,7 @@
 
 import au.com.bytecode.opencsv.CSVReader;
 import au.com.bytecode.opencsv.CSVWriter;
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
 import se.walkercrou.places.GooglePlaces;
 import se.walkercrou.places.Param;
 import se.walkercrou.places.Place;
@@ -18,11 +15,8 @@ import java.util.List;
  * Created by pchen on 1/30/2017.
  */
 public class PlaceToAddress {
-<<<<<<< HEAD
     private String apiKey="";
     public String getApiKey(){return apiKey;}
-=======
->>>>>>> origin/master
     private List<String[]> inFile;
     private void loadCsv(String filename){
         try {
@@ -47,11 +41,8 @@ public class PlaceToAddress {
 
     }
     public static void main(String[] args) {
-<<<<<<< HEAD
         GooglePlaces client= new GooglePlaces(new PlaceToAddress().getApiKey());
-=======
-        GooglePlaces client= new GooglePlaces("AIzaSyDL1IpxsgU4d2u5VRHSPxognEhqcm-0ah4");
->>>>>>> origin/master
+
         PlaceToAddress placelist = new PlaceToAddress();
         placelist.loadCsv("/home/patrick/PlacesToAddress/src/FacilityContacts.csv");
 
